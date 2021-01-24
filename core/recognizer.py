cd5103eeb932f0a2796d6c2419c029dc510c0a71
ShogunHirei <hyertega@hotmail.com>
2021-01-23 22:25:04 -0300

    Reconhecimento de caracteres completo - Separação da caixa de mensagens - Separação de mensagens - Remoção de figuras e outros elementos gráficos que não sejam caracteres - Instalação do pytesseract - Reconhecimento de texto completo
    TODO: Criar projeto Django
          - Verificar buildpack no Heroku para pytesseract
          Criar sistema de verificação por usuário
          - Estabelecer relações entre mentores (R4) e usuários
          - Estabelecer sistema de verificação de cota
          Criar sistema de validação de print
          - Verificação de chat

--- core/recognizer.py
+++ core/recognizer.py
@@ -13,7 +13,8 @@ import numpy as np
 import cv2 as cv
 import pdb
 from matplotlib import pyplot as plt
-
+from PIL import Image
+import pytesseract
 
 parser = argparse.ArgumentParser()
 
@@ -53,14 +54,99 @@ else:
     num = np.random.randint(1,high=8)
 FILEPATH = f'../data/raw_data/img{num}.jpeg'
 
+def letters_clean(img):
+    """
+        File: recognizer.py
+        Function Name: letters_clean
+        Summary: Detecção Canny para os caracteres
+        Description: Parametros utilizados para limpar a imagem real e destacar os caracteres
+
+        img -> imagem sem manipulações
+    """
+    new_img = cv.Canny(img, 10, 55, apertureSize=3, L2gradient=True)
+    return new_img
+    
+
+def  line_check(img, of_img):
+    """
+        File: recognizer.py
+        Function Name: line_check
+        Summary: Detectar linhas usando trasnformada Hough
+
+        img -> manipulated image to identify lines
+        of_img -> image to draw lines
+    """
+    lines = cv.HoughLinesP(img, 5, np.pi/90, 300, minLineLength=400, maxLineGap=1)
+
+    rgb_img = cv.cvtColor(of_img, cv.COLOR_GRAY2BGR)
+
+    if not lines is None:
+        print('Linhas', len(lines))
+        for line in lines:
+            x1,y1,x2,y2 = line[0]
+            cv.line(of_img,(x1,y1),(x2,y2),(255,255,0),2)
+    else:
+        print('Foi mal camarada!')
+
+    return of_img, lines
+
+    
+
+
+def c_rects(img, img_res, *args, draw=True, limits=10, minArea=True):
+    """
+        File: recognizer.py
+        Function Name: c_rects
+        Summary: Find countours and draw Rectangles
+
+        img -> Imagem com manipulações para encontrar os contornor
+        img_res -> imagem na qual serão desenhados os retângulos
+    """
+    if not bool(args):
+        args = [cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE,]
+
+    contours, hier = cv.findContours(img, *args)
+
+    if limits:
+        greaterArea = sorted([ (idx, cv.contourArea(cont)) for idx,cont in enumerate(contours) ], 
+                          key=lambda x: x[1], reverse=True)[1:10]
+        contours = [ contours[i] for i, a in greaterArea]
+
+    if draw:
+        for c in contours:
+            # find bounding box coordinates
+            x,y,w,h = cv.boundingRect(c)
+            cv.rectangle(img_res, (x,y), (x+w, y+h), (255, 255, 0), 2)
+
+            if minArea:
+                # find minimum area
+                rect = cv.minAreaRect(c)
+                # calculate coordinates of the minimum area rectangle
+                box = cv.boxPoints(rect)
+                # normalize coordinates to integers
+                box = np.int0(box)
+                # draw contours
+                cv.drawContours(img_res, [box], 0, (255, 255, 0), 5)
+
+    return contours, hier
+
 
 # Para verificação do char usar opções 
 # -c 80 20 -dl -40 0
 # Necessário para posterior tratamento das condições do chat
 def main(path, option, prcts, dy):
+    # TODO: Desenhar na imagem: Retangulo de caixa de mensagens - FEITO!
+    #                           Retângulo de cada mensagem - FEITO
+    #   
+    #       Remover desenhos e circundar verdadeira região de interesse (TROI)
+    #           Sugestões:
+    #                   -> Usar dilate e GaussianBlur e circundar região com desenho
+    #                   -> Borrar região e extrair texto
+    #                   -> usar máscara para desenhos em imagem manipulada
+    #                   
+
     # Loading image array
     img_arr = cv.imread(path, 0)
-    # img_arr = cv.cvtColor(img_arr, cv.COLOR_BGR2GRAY)
 
     # Image Manipulation
     kernel = np.ones((2,2), np.uint8)
@@ -80,26 +166,7 @@ def main(path, option, prcts, dy):
     # res_img = cv.adaptiveThreshold(res_img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
     res_img = cv.dilate(blur, kernel, iterations=7)
 
-    # # Avaliando histograma das imagens
-    # plt.hist(res_img.ravel(), 256)
-    # plt.show()
-
-
-    # # Identificação de linhas no dentro das mensagens
-    # lines = cv.HoughLinesP(res_img, 5, np.pi/(180), 400, minLineLength=100,maxLineGap=10)
-    # for line in lines:
-        # x1, y1, x2, y2 = line[0]
-        # # rho,theta = line[0]
-        # # a = np.cos(theta)
-        # # b = np.sin(theta)
-        # # x0 = a*rho
-        # # y0 = b*rho
-        # # x1 = int(x0 + 1000*(-b))
-        # # y1 = int(y0 + 1000*(a))
-        # # x2 = int(x0 - 1000*(-b))
-        # # y2 = int(y0 - 1000*(a))
-        # cv.line(img_arr,(x1,y1),(x2,y2),(0,0,255),2)
-
+    pimg= res_img.copy()
 
     # # Função encontra satisfatoriamente todos os caracteres da imagem
     # #   mas também circunda elementos indesejados
@@ -116,7 +183,7 @@ def main(path, option, prcts, dy):
     print('Maior contorno', bigArea)
 
     # Convertendo imagem para RGB para fazer contorno colorido
-    rgb_img = cv.cvtColor(img_arr, cv.COLOR_GRAY2BGR)
+    # rgb_img = cv.cvtColor(img_arr, cv.COLOR_GRAY2BGR)
 
     # O segundo maior contorno, em área, será o que conterá provavelmente
     #   a caixa de mensagens, é possível usá-lo para extrair o fundo da imagem
@@ -125,7 +192,7 @@ def main(path, option, prcts, dy):
     rect = cv.minAreaRect(contours[bigArea[0][0]])
     box = np.int0(cv.boxPoints(rect))
     # Desenhar o contorno agora é opcional
-    cv.drawContours(rgb_img, [box], 0, (85), 3)
+    # cv.drawContours(rgb_img, [box], 0, (85), 3)
 
     # pdb.set_trace()
     # Iniciando extração de zona fora do ROI
@@ -136,31 +203,80 @@ def main(path, option, prcts, dy):
     x1, x2 = ROI_x.min(), ROI_x.max()
 
     img_arr = img_arr[y1:y2, x1:x2]
+    res_img = res_img[y1:y2, x1:x2] 
 
-    # for c in contours:
-        # # find bounding box coordinates
-        # x,y,w,h = cv.boundingRect(c)
-        # cv.rectangle(res_img, (x,y), (x+w, y+h), (255, 255, 0), 2)
-        # # find minimum area
-        # rect = cv.minAreaRect(c)
-        # # calculate coordinates of the minimum area rectangle
-        # box = cv.boxPoints(rect)
-        # # normalize coordinates to integers
-        # box = np.int0(box)
-        # # draw contours
-        # cv.drawContours(img_arr, [box], 0, (255, 255, 0), 5)
+    # Desenhar um retângulo ao redor de cada mensagem
 
-    # TODO: Desenhar na imagem: Retangulo de caixa de mensagens - FEITO!
-    #                           Retângulo de cada mensagem - FEITO
-    #   
-    #       Remover desenhos e circundar verdadeira região de interesse (TROI)
-    #           Sugestões:
-    #                   -> Usar dilate e GaussianBlur e circundar região com desenho
-    #                   -> Borrar região e extrair texto
-    #                   -> usar máscara para desenhos em imagem manipulada
-    #                   
+    # Remover símbolos de carta e caixa de marcação com base em porcentagem
+    #   remoção apenas na direção x
+    nx1 = int( (x2-x1) * 0.165)
+    nx2 = int( (x2-x1) * 0.995)
+    res_img = res_img[:, nx1:nx2]
+    img_arr = img_arr[:, nx1:nx2]
 
+    # Atualizando imagem manipulada para novas manipulações
+    res_img = img_arr.copy()
 
+    
+    # Borrar imagem para limitar 
+    res_img = cv.GaussianBlur(res_img, (7,7), 0)
+    res_img = cv.adaptiveThreshold(res_img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, 1)
+    # Erode
+    kernel2 = np.full((7,7), 1, dtype=np.uint8)
+    res_img = cv.dilate(res_img, kernel, iterations=3)
+
+    # res_img = cv.Sobel(res_img,cv.CV_64F,0,1,ksize=5)
+    # res_img = np.uint8( np.absolute(res_img))
+
+    # # Identificar linhas para limitar as mensagens
+    img_arr, lines = line_check(~res_img.copy(), img_arr)
+
+    # Linhas determinadas com sucesso
+    # A função HougLinesP returna um array com os ponto da linha (x1,y1,x2,y2)  
+    # Para isolar as mensagens usar a distancia entre y1 de cada linha e cortar a
+    #   imagem original nesses pedaços
+
+    dy = []
+    p_y1 = 0
+
+    # Filtrar apenas pontos y1
+    lines = lines[:, 0, 1]
+    # Organizar linhas em ordem decrescente
+    lines.sort()
+
+    # Inserir elemento nulo para manter integridade entre diferença de pontos
+    lines = np.concatenate((np.zeros(1,), lines))
+    # Obter diferença entre pontos para verificar se linhas não estão muito proximas
+    lines = np.diff(lines)
+
+    # Diferença de ponto ordenado permite filtragem para eliminar linhas sobrepostas
+    cutPoints = [ int(pnt) for pnt in lines if pnt > 10]
+    
+    sp = len(cutPoints)
+    old_pnt = 0
+    msgs = []
+    for idx, pnt in enumerate(cutPoints, 1):
+        # Retirar estrela para facilitar para Tesseract
+        temp_img = img_arr[old_pnt:pnt+old_pnt, :].copy()
+
+        y, x = temp_img.shape
+        y = int(y*0.325)
+        x = int(x*0.8)
+
+        temp_img[ y:, x: ] = 0
+        msgs.append(temp_img)
+
+        old_pnt = pnt
+
+    # Mensagens filtradas!
+    img_arr = msgs[-1]
+
+    # Usando pytesseract contra a minha vontade
+    print(*[pytesseract.image_to_string(msg) for msg in msgs])
+
+    # Detectado texto em mensagens.
+
+    # Passar para a análise de acordo com determinados usuários
 
 
     # Image result show
